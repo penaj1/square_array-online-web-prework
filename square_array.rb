e222c7c098216cecbd21e76e9d92043713e71603
Janice Pena <jipena94@gmail.com>
2019-04-08 02:17:22 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   numbers = [1,2,3]
    
-  calls_on.each do |numbers|
+  square_array.each do |numbers|
   puts "Taking out #{apple}"
   apples_taken_out += 1
 end
